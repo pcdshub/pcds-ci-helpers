cf8a4e361d8bec181a5a0fd6c6b7d56d9378fd1f
K Lauer <klauer@slac.stanford.edu>
2019-12-18 15:58:27 -0800

    FIX: python linter header + footer

--- travis/python_linter.sh
+++ travis/python_linter.sh
@@ -4,8 +4,27 @@
 #   * source `python_linter.sh` on Travis CI
 
 pip install flake8
-echo ""
-printf '=%.0s' {1..100}
-echo "Python linter results:"
-flake8 $LINT_PYTHON
-exit  # exits with the status of flake8
+linter_header=$(cat <<'EOF'
+=====================
+Python linter results
+=====================
+
+EOF
+)
+
+linter_footer=$(cat <<'EOF'
+=====================
+EOF
+)
+
+echo "$linter_header"
+echo "$ flake8 $LINT_PYTHON"
+flake8 $LINT_PYTHON; FLAKE8_EXIT_CODE=$?
+echo "$linter_footer"
+if [ $FLAKE8_EXIT_CODE -ne 0 ]; then
+    echo "Exit code: $FLAKE8_EXIT_CODE" 
+fi
+
+sleep 1.0
+
+exit $FLAKE8_EXIT_CODE

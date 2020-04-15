a46acfca9cac82c5153f2670427bcb96965ed529
Zachary Lentz <zlentz@slac.stanford.edu>
2020-04-15 16:07:33 -0700

    ENH: include file names and line text in failure output

--- travis/tc3_style/check.sh
+++ travis/tc3_style/check.sh
@@ -6,25 +6,31 @@ exit_code=0
 
 _header "Checking that source code files do not contain tabs..."
 
-tab_lines=$(./files.sh | xargs awk '/\t/' | wc -l)
-if [ "${tab_lines}" -ne 0 ]; then
-  echo "Found ${tab_lines} lines with tabs"
+tab_lines=$(style/files.sh | xargs egrep $'\t')
+tab_count=$(echo "${tab_lines}" | wc -l)
+if [ "${tab_count}" -ne 0 ]; then
+  echo "Found ${tab_count} lines with tabs"
+  echo "${tab_lines}"
   exit_code=1
 fi
 
 _header "Checking for lines with trailing whitespace..."
 
-bad_whitespace_lines=$(./files.sh | xargs egrep $' +$| +\r$' | wc -l)
-if [ "${bad_whitespace_lines}" -ne 0 ]; then
-  echo "Found ${bad_whitespace_lines} lines with trailing whitespace"
+bad_whitespace_lines=$(style/files.sh | xargs egrep $' +$| +\r$')
+bad_whitespace_count=$(echo "${bad_whitespace_lines}" | wc -l)
+if [ "${bad_whitespace_count}" -ne 0 ]; then
+  echo "Found ${bad_whitespace_count} lines with trailing whitespace"
+  echo "${bad_whitespace_lines}"
   exit_code=2
 fi
 
 _header "Checking for TwinCAT misconfiguration (Line IDs)..."
 
-lineid_lines=$(./files.sh | xargs grep 'LineId' | wc -l)
-if [ "${lineid_lines}" -ne 0 ]; then
-  echo "Found ${lineid_lines} lines with same-file debug line ids (fix your twincat config)"
+lineid_lines=$(./files.sh | xargs grep 'LineId')
+lineid_count=$(echo "${lineid_lines}" | wc -l)
+if [ "${lineid_count}" -ne 0 ]; then
+  echo "Found ${lineid_count} lines with same-file debug line ids (fix your twincat config)"
+  echo "${lineid_lines}"
   exit_code=3
 fi
 

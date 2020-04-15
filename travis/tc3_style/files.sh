1a94cd55f18f552679606f96877f437d58561e13
Zachary Lentz <zlentz@slac.stanford.edu>
2020-04-15 16:27:15 -0700

    ENH: allow filename regex for style check skips

--- travis/tc3_style/files.sh
+++ travis/tc3_style/files.sh
@@ -2,4 +2,9 @@
 
 source $CI_HELPER_PATH/travis/settings.sh
 
-find $TWINCAT_PROJECT_ROOT -regextype posix-extended -regex '.*\.(TcPOU|TcDUT|TcGVL)$'
+all_files=$(find $TWINCAT_PROJECT_ROOT -regextype posix-extended -regex '.*\.(TcPOU|TcDUT|TcGVL)$')
+if [ -z "${TWINCAT_STYLE_EXCLUDE}" ]; then
+  echo "${all_files}"
+else
+  echo "${all_files}" | egrep -v "${TWINCAT_STYLE_EXCLUDE}"
+fi

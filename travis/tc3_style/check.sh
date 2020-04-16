94a4d5c9a66a1a707f41340ab7b7b84c5d942967
Zachary Lentz <zlentz@slac.stanford.edu>
2020-04-16 09:40:17 -0700

    BUG: count zero as zero, not one

--- travis/tc3_style/check.sh
+++ travis/tc3_style/check.sh
@@ -7,8 +7,8 @@ exit_code=0
 _header "Checking that source code files do not contain tabs..."
 
 tab_lines=$(./files.sh | xargs egrep $'\t')
-tab_count=$(echo "${tab_lines}" | wc -l)
-if [ "${tab_count}" -ne 0 ]; then
+if [ -n "${tab_lines}" ]; then
+  tab_count=$(echo "${tab_lines}" | wc -l)
   echo "Found ${tab_count} lines with tabs"
   echo "${tab_lines}"
   exit_code=1
@@ -17,8 +17,8 @@ fi
 _header "Checking for lines with trailing whitespace..."
 
 bad_whitespace_lines=$(./files.sh | xargs egrep $' +$| +\r$')
-bad_whitespace_count=$(echo "${bad_whitespace_lines}" | wc -l)
-if [ "${bad_whitespace_count}" -ne 0 ]; then
+if [ -n "${bad_whitespace_lines}" ]; then
+  bad_whitespace_count=$(echo "${bad_whitespace_lines}" | wc -l)
   echo "Found ${bad_whitespace_count} lines with trailing whitespace"
   echo "${bad_whitespace_lines}"
   exit_code=2
@@ -27,8 +27,8 @@ fi
 _header "Checking for TwinCAT misconfiguration (Line IDs)..."
 
 lineid_lines=$(./files.sh | xargs grep 'LineId')
-lineid_count=$(echo "${lineid_lines}" | wc -l)
-if [ "${lineid_count}" -ne 0 ]; then
+if [ -n "${lineid_lines}" ]; then
+  lineid_count=$(echo "${lineid_lines}" | wc -l)
   echo "Found ${lineid_count} lines with same-file debug line ids (fix your twincat config)"
   echo "${lineid_lines}"
   exit_code=3
